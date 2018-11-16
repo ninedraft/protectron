027f0229b53c9630816fb5368d08ed1f11ae70e0
ninedraft <clamores.pro@gmail.com>
2018-11-16 23:12:40 +0300

    add cli

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,25 +1,40 @@
 package cli
 
 import (
+	"log"
+
+	"github.com/ninedraft/protectron/pkg/bot"
 	gpflag "github.com/octago/sflags/gen/gpflag"
 	cobra "github.com/spf13/cobra"
 )
 
 type Flags struct {
-	// flag definitions here
 	// https://github.com/octago/sflags#flags-based-on-structures------
+	UseLocalTorProxy bool
+	Token            string
 }
 
 func Command() *cobra.Command {
-	var flags Flags // default values can be defined here
+	var flags = Flags{}
 	var cmd = &cobra.Command{
 		Use: "command",
 		Run: func(cmd *cobra.Command, args []string) {
-			// write your code here
+			var proxy *bot.SOCKS5ProxyConfig
+			if flags.UseLocalTorProxy {
+				proxy = &bot.SOCKS5ProxyConfig{
+					Host: "localhost",
+					Port: 9050,
+				}
+			}
+			var protector = bot.New(flags.Token, proxy)
+			if err := protector.Run(); err != nil {
+				log.Fatal(err)
+			}
 		},
 	}
 	if err := gpflag.ParseTo(&flags, cmd.PersistentFlags()); err != nil {
 		panic(err)
 	}
+
 	return cmd
 }
