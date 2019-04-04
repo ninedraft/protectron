ac9d61cc4bb6f7dc61a7dff866f01db12a0e64c2
ninedraft <clamores.pro@gmail.com>
2019-04-04 17:15:42 +0300

    refactor main routine

--- pkg/bot/bot.go
+++ pkg/bot/bot.go
@@ -1,35 +1,18 @@
 package bot
 
 import (
-	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
-	"golang.org/x/net/proxy"
-	"gopkg.in/telegram-bot-api.v4"
+	"github.com/ninedraft/protectron/pkg/proxy"
+	tgbotapi "gopkg.in/telegram-bot-api.v4"
 )
 
 type Bot struct {
 	token string
 	proxy *SOCKS5ProxyConfig
-}
-
-type SOCKS5ProxyConfig struct {
-	Host     string
-	Port     int
-	Username string
-	Password string
-}
-
-func (proxy *SOCKS5ProxyConfig) NoSecurity() bool {
-	return proxy.Password == "" && proxy.Username == ""
-}
-
-func New(token string, proxy *SOCKS5ProxyConfig) Bot {
-	return Bot{
-		token: token,
-		proxy: proxy,
-	}
 }
 
 func (bot Bot) Run() error {
@@ -45,24 +28,17 @@ func (bot Bot) Run() error {
 		}
 	} else {
 		log.Printf("using proxy")
-		var auth *proxy.Auth
-		if !bot.proxy.NoSecurity() {
-			auth = &proxy.Auth{
-				User:     bot.proxy.Username,
-				Password: bot.proxy.Password,
-			}
-		}
-		var dialer, errSOCKS5 = proxy.SOCKS5("tcp",
-			fmt.Sprintf("%s:%d", bot.proxy.Host, bot.proxy.Port),
-			auth, proxy.Direct)
-		if errSOCKS5 != nil {
-			return errSOCKS5
+		var client, errConnectProxy = proxy.SOCKS5(bot.proxy.Address(), proxy.Auth{
+			User:     bot.proxy.Username,
+			Password: bot.proxy.Password,
+		})
+		if errConnectProxy != nil {
+			logErr("unable to use proxy: %v", errConnectProxy)
+			os.Exit(1)
 		}
 		api, errAPI = tgbotapi.NewBotAPIWithClient(bot.token,
 			&http.Client{
-				Transport: &http.Transport{
-					Dial: dialer.Dial,
-				},
+				Transport: client.Transport,
 			})
 		if errAPI != nil {
 			return errAPI
@@ -78,48 +54,41 @@ func (bot Bot) Run() error {
 	}
 	log.Printf("listening for updates")
 	for update := range updates {
-		switch {
-		case (update.Message != nil && update.Message.ForwardFromChat != nil):
-			go func() {
-				defer func() {
-					var recMsg = recover()
-					if recMsg != nil {
-						log.Printf("[PANIC] on message from %q \n%v", update.Message.From, recMsg)
-					}
-				}()
-				var userID = update.Message.From.ID
-				var chatID = update.Message.Chat.ID
-				var messageID = update.Message.MessageID
-				log.Printf("[INFO] repost from @%s, engaging into battle", update.Message.ForwardFromChat.UserName)
-				var resp, errKick = api.KickChatMember(tgbotapi.KickChatMemberConfig{
-					ChatMemberConfig: tgbotapi.ChatMemberConfig{
-						UserID: userID,
-						ChatID: chatID,
-					},
-					UntilDate: -1,
-				})
-				if errKick != nil {
-					log.Printf("[ERROR] %s", errKick)
-					return
-				}
-				if !resp.Ok {
-					log.Printf("[ERROR] %s", resp.Description)
-					return
-				}
-				var respDelete, errDelete = api.DeleteMessage(tgbotapi.DeleteMessageConfig{
-					ChatID:    chatID,
-					MessageID: messageID,
-				})
-				if errDelete != nil {
-					log.Printf("[ERROR] %s", errDelete)
-					return
-				}
-				if !respDelete.Ok {
-					log.Printf("[ERROR] %s", respDelete.Description)
-					return
-				}
-			}()
-		}
+		go processMessage(botContext{
+			Bot:           api,
+			Msg:           update.Message,
+			HostWhitelist: strSet([]string{}),
+			ChatWhitelist: func(id int64) bool { return true },
+		})
 	}
 	return nil
+}
+
+type botContext struct {
+	Bot           *tgbotapi.BotAPI
+	Msg           *tgbotapi.Message
+	ChatWhitelist idFilter
+	HostWhitelist strFilter
+}
+
+type SOCKS5ProxyConfig struct {
+	Host     string
+	Port     int
+	Username string
+	Password string
+}
+
+func (socks5 SOCKS5ProxyConfig) Address() string {
+	return socks5.Host + ":" + strconv.Itoa(socks5.Port)
+}
+
+func (proxy *SOCKS5ProxyConfig) NoSecurity() bool {
+	return proxy.Password == "" && proxy.Username == ""
+}
+
+func New(token string, proxy *SOCKS5ProxyConfig) Bot {
+	return Bot{
+		token: token,
+		proxy: proxy,
+	}
 }
