c7195629aaab354fb91bd593e039aa9b8a0eddd4
ninedraft <clamores.pro@gmail.com>
2019-04-09 21:24:36 +0300

    change id type to int and add method userIsTooYoung

--- pkg/protectron/user_registry.go
+++ pkg/protectron/user_registry.go
@@ -7,30 +7,39 @@ import (
 
 type registry struct {
 	mu    sync.RWMutex
-	users map[int64]time.Time
+	users map[int]time.Time
 	stop  chan struct{}
 }
 
 func newRegistry(vacuumDelta time.Duration) *registry {
 	var reg = &registry{
-		users: make(map[int64]time.Time),
+		users: make(map[int]time.Time),
 		stop:  make(chan struct{}),
 	}
 	startVacuum(vacuumDelta, reg)
 	return reg
 }
 
-func (reg *registry) addUser(id int64) {
+func (reg *registry) addUser(id int) {
 	reg.mu.Lock()
 	defer reg.mu.Unlock()
 	reg.users[id] = time.Now()
 }
 
-func (reg *registry) getUserJoinTime(id int64) (time.Time, bool) {
+func (reg *registry) getUserJoinTime(id int) (time.Time, bool) {
 	reg.mu.RLock()
 	defer reg.mu.RUnlock()
 	var joined, ok = reg.users[id]
 	return joined, ok
+}
+
+func (reg *registry) userIsTooYoung(delta time.Duration, id int) bool {
+	var joined, ok = reg.getUserJoinTime(id)
+	if !ok {
+		return false
+	}
+	var banHorizon = time.Now().Add(-delta)
+	return joined.After(banHorizon)
 }
 
 func (reg *registry) stopVacuum() {
